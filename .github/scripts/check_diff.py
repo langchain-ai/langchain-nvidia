6cc16b2529d8442b2a54998dcbc5d731b1e9aff7
Erick Friis <erick@langchain.dev>
2024-02-28 14:11:49 -0800

    packages

---
+++ .github/scripts/check_diff.py
@@ -0,0 +1,36 @@
+import json
+import sys
+import os
+
+LANGCHAIN_DIRS = {
+    "libs/genai",
+    "libs/vertexai",
+}
+
+if __name__ == "__main__":
+    files = sys.argv[1:]
+    dirs_to_run = set()
+
+    if len(files) == 300:
+        # max diff length is 300 files - there are likely files missing
+        raise ValueError("Max diff reached. Please manually run CI on changed libs.")
+
+    for file in files:
+        if any(
+            file.startswith(dir_)
+            for dir_ in (
+                ".github/workflows",
+                ".github/tools",
+                ".github/actions",
+                ".github/scripts/check_diff.py",
+            )
+        ):
+            dirs_to_run.update(LANGCHAIN_DIRS)
+        elif "libs/genai" in file:
+            dirs_to_run.update({"libs/genai"})
+        elif "libs/vertexai" in file:
+            dirs_to_run.update({"libs/vertexai"})
+        else:
+            pass
+    json_output = json.dumps(list(dirs_to_run))
+    print(f"dirs-to-run={json_output}")
---
+++ README.md
@@ -0,0 +1,5 @@
+# 🦜️🔗 LangChain NVIDIA
+
+This repository contains two packages with NVIDIA integrations with LangChain:
+- [langchain-nvidia-ai-endpoints](https://pypi.org/project/langchain-nvidia-ai-endpoints/) integrates [NVIDIA AI Foundation Models and Endpoints](https://www.nvidia.com/en-us/ai-data-science/foundation-models/).
+- [langchain-nvidia-trt](https://pypi.org/project/langchain-nvidia-trt/) implements integrations of NVIDIA [TensorRT](https://developer.nvidia.com/tensorrt) models.
