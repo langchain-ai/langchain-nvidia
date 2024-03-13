a1f35f9492fb2d89cfc872a33e1faaeeb8351319
Matthew Farrellee <matt@cs.wisc.edu>
2024-03-13 07:47:19 -0400

    make test_chat_ai_endpoints_* tests work with model sweep

--- libs/ai-endpoints/tests/integration_tests/test_chat_models.py
+++ libs/ai-endpoints/tests/integration_tests/test_chat_models.py
@@ -7,10 +7,18 @@ from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage
 from langchain_nvidia_ai_endpoints.chat_models import ChatNVIDIA
 
 
-def test_chat_ai_endpoints() -> None:
+#
+# we setup an --all-models flag in conftest.py, when passed it configures chat_model and image_in_model to be all
+# available models of type chat or image_in
+#
+# note: currently --all-models only works with the default mode because different modes may have different available models
+#
+
+
+def test_chat_ai_endpoints(chat_model) -> None:
     """Test ChatNVIDIA wrapper."""
     chat = ChatNVIDIA(
-        model="llama2_13b",
+        model=chat_model,
         temperature=0.7,
     )
     message = HumanMessage(content="Hello")
@@ -25,9 +33,9 @@ def test_chat_ai_endpoints_model() -> None:
     assert chat.model == "mistral"
 
 
-def test_chat_ai_endpoints_system_message() -> None:
+def test_chat_ai_endpoints_system_message(chat_model) -> None:
     """Test wrapper with system message."""
-    chat = ChatNVIDIA(model="llama2_13b", max_tokens=36)
+    chat = ChatNVIDIA(model=chat_model, max_tokens=36)
     system_message = SystemMessage(content="You are to chat with the user.")
     human_message = HumanMessage(content="Hello")
     response = chat([system_message, human_message])
@@ -38,34 +46,34 @@ def test_chat_ai_endpoints_system_message() -> None:
 ## TODO: Not sure if we want to support the n syntax. Trash or keep test
 
 
-def test_ai_endpoints_streaming() -> None:
+def test_ai_endpoints_streaming(chat_model) -> None:
     """Test streaming tokens from ai endpoints."""
-    llm = ChatNVIDIA(model="llama2_13b", max_tokens=36)
+    llm = ChatNVIDIA(model=chat_model, max_tokens=36)
 
     for token in llm.stream("I'm Pickle Rick"):
         assert isinstance(token.content, str)
 
 
-async def test_ai_endpoints_astream() -> None:
+async def test_ai_endpoints_astream(chat_model) -> None:
     """Test streaming tokens from ai endpoints."""
-    llm = ChatNVIDIA(model="llama2_13b", max_tokens=35)
+    llm = ChatNVIDIA(model=chat_model, max_tokens=35)
 
     async for token in llm.astream("I'm Pickle Rick"):
         assert isinstance(token.content, str)
 
 
-async def test_ai_endpoints_abatch() -> None:
+async def test_ai_endpoints_abatch(chat_model) -> None:
     """Test streaming tokens."""
-    llm = ChatNVIDIA(model="llama2_13b", max_tokens=36)
+    llm = ChatNVIDIA(model=chat_model, max_tokens=36)
 
     result = await llm.abatch(["I'm Pickle Rick", "I'm not Pickle Rick"])
     for token in result:
         assert isinstance(token.content, str)
 
 
-async def test_ai_endpoints_abatch_tags() -> None:
+async def test_ai_endpoints_abatch_tags(chat_model) -> None:
     """Test batch tokens."""
-    llm = ChatNVIDIA(model="llama2_13b", max_tokens=55)
+    llm = ChatNVIDIA(model=chat_model, max_tokens=55)
 
     result = await llm.abatch(
         ["I'm Pickle Rick", "I'm not Pickle Rick"], config={"tags": ["foo"]}
@@ -74,43 +82,31 @@ async def test_ai_endpoints_abatch_tags() -> None:
         assert isinstance(token.content, str)
 
 
-def test_ai_endpoints_batch() -> None:
+def test_ai_endpoints_batch(chat_model) -> None:
     """Test batch tokens."""
-    llm = ChatNVIDIA(model="llama2_13b", max_tokens=60)
+    llm = ChatNVIDIA(model=chat_model, max_tokens=60)
 
     result = llm.batch(["I'm Pickle Rick", "I'm not Pickle Rick"])
     for token in result:
         assert isinstance(token.content, str)
 
 
-async def test_ai_endpoints_ainvoke() -> None:
+async def test_ai_endpoints_ainvoke(chat_model) -> None:
     """Test invoke tokens."""
-    llm = ChatNVIDIA(model="llama2_13b", max_tokens=60)
+    llm = ChatNVIDIA(model=chat_model, max_tokens=60)
 
     result = await llm.ainvoke("I'm Pickle Rick", config={"tags": ["foo"]})
     assert isinstance(result.content, str)
 
 
-def test_ai_endpoints_invoke() -> None:
+def test_ai_endpoints_invoke(chat_model) -> None:
     """Test invoke tokens."""
-    llm = ChatNVIDIA(model="llama2_13b", max_tokens=60)
+    llm = ChatNVIDIA(model=chat_model, max_tokens=60)
 
     result = llm.invoke("I'm Pickle Rick", config=dict(tags=["foo"]))
     assert isinstance(result.content, str)
 
 
-#
-# we setup an --all-models flag in conftest.py, when passed it configures chat_model and image_in_model to be all
-# available models of type chat or image_in
-#
-# note: currently --all-models only works with the default mode because different modes may have different available models
-#
-
-def test_chat_models(chat_model) -> None:
-    chat = ChatNVIDIA(model=chat_model)
-    response = chat.invoke([HumanMessage(content="Hello")])
-    assert isinstance(response, BaseMessage)
-    assert isinstance(response.content, str)
 
 
 def test_image_in_models(image_in_model) -> None:
