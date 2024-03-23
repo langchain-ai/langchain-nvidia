0510cd06a008aa7d10b29c0a147dc479dac023ea
Matthew Farrellee <matt@cs.wisc.edu>
2024-03-23 07:58:09 -0400

    validate NVIDIAEmbeddings.embed_documents input type is list[str]
    resolves https://github.com/langchain-ai/langchain-nvidia/issues/8

---
+++ libs/ai-endpoints/tests/unit_tests/test_embeddings.py
@@ -0,0 +1,82 @@
+from typing import Generator
+
+import pytest
+from requests_mock import Mocker
+
+from langchain_nvidia_ai_endpoints import NVIDIAEmbeddings
+
+
+@pytest.fixture
+def embedding(requests_mock: Mocker) -> Generator[NVIDIAEmbeddings, None, None]:
+    model = "mock-model"
+    requests_mock.get(
+        "https://api.nvcf.nvidia.com/v2/nvcf/functions",
+        json={
+            "functions": [
+                {
+                    "id": "ID",
+                    "ncaId": "NCA-ID",
+                    "versionId": "VERSION-ID",
+                    "name": model,
+                    "status": "ACTIVE",
+                    "ownedByDifferentAccount": True,
+                    "apiBodyFormat": "CUSTOM",
+                    "healthUri": "/v2/health/ready",
+                    "createdAt": "0000-00-00T00:00:00.000Z",
+                }
+            ]
+        },
+    )
+    requests_mock.post(
+        "https://api.nvcf.nvidia.com/v2/nvcf/pexec/functions/ID",
+        json={
+            "data": [
+                {
+                    "embedding": [
+                        0.1,
+                        0.2,
+                        0.3,
+                    ],
+                    "index": 0,
+                }
+            ],
+            "usage": {"prompt_tokens": 8, "total_tokens": 8},
+        },
+    )
+    yield NVIDIAEmbeddings(model=model, nvidia_api_key="a-bogus-key")
+
+
+def test_embed_documents_negative_input_int(embedding: NVIDIAEmbeddings) -> None:
+    documents = 1
+    with pytest.raises(ValueError):
+        embedding.embed_documents(documents)  # type: ignore
+
+
+def test_embed_documents_negative_input_float(embedding: NVIDIAEmbeddings) -> None:
+    documents = 1.0
+    with pytest.raises(ValueError):
+        embedding.embed_documents(documents)  # type: ignore
+
+
+def test_embed_documents_negative_input_str(embedding: NVIDIAEmbeddings) -> None:
+    documents = "subscriptable string, not a list"
+    with pytest.raises(ValueError):
+        embedding.embed_documents(documents)  # type: ignore
+
+
+def test_embed_documents_negative_input_list_int(embedding: NVIDIAEmbeddings) -> None:
+    documents = [1, 2, 3]
+    with pytest.raises(ValueError):
+        embedding.embed_documents(documents)  # type: ignore
+
+
+def test_embed_documents_negative_input_list_float(embedding: NVIDIAEmbeddings) -> None:
+    documents = [1.0, 2.0, 3.0]
+    with pytest.raises(ValueError):
+        embedding.embed_documents(documents)  # type: ignore
+
+
+def test_embed_documents_negative_input_list_mixed(embedding: NVIDIAEmbeddings) -> None:
+    documents = ["1", 2.0, 3]
+    with pytest.raises(ValueError):
+        embedding.embed_documents(documents)  # type: ignore
