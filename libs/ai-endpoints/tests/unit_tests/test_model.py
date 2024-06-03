381d4609aa409256813565fe39bde380ad4f71d0
Matthew Farrellee <matt@cs.wisc.edu>
2024-06-03 09:21:14 -0400

    add unit tests for handling of models as they appear in the model table and listing

--- libs/ai-endpoints/tests/unit_tests/test_model.py
+++ libs/ai-endpoints/tests/unit_tests/test_model.py
@@ -1,6 +1,29 @@
 import pytest
+from requests_mock import Mocker
 
 from langchain_nvidia_ai_endpoints._statics import MODEL_TABLE
+
+
+@pytest.fixture
+def known_unknown() -> str:
+    return "mock-model"
+
+
+@pytest.fixture(autouse=True)
+def mock_v1_models(requests_mock: Mocker, known_unknown: str) -> None:
+    requests_mock.get(
+        "https://integrate.api.nvidia.com/v1/models",
+        json={
+            "data": [
+                {
+                    "id": known_unknown,
+                    "object": "model",
+                    "created": 1234567890,
+                    "owned_by": "OWNER",
+                },
+            ]
+        },
+    )
 
 
 @pytest.mark.parametrize(
@@ -13,11 +36,53 @@ from langchain_nvidia_ai_endpoints._statics import MODEL_TABLE
     ],
 )
 def test_aliases(public_class: type, alias: str) -> None:
-    with pytest.warns(UserWarning):
-        x = public_class(model=alias)
+    """
+    Test that the aliases for each model in the model table are accepted
+    with a warning about deprecation of the alias.
+    """
+    with pytest.warns(UserWarning) as record:
+        x = public_class(model=alias, nvidia_api_key="a-bogus-key")
         assert x.model == x._client.model
+    assert isinstance(record[0].message, Warning)
+    assert "deprecated" in record[0].message.args[0]
 
 
-def test_unavailable(public_class: type) -> None:
-    with pytest.raises(ValueError):
-        public_class(model="not-a-real-model")
+def test_known(public_class: type) -> None:
+    """
+    Test that a model in the model table will be accepted.
+    """
+    # find a model that matches the public_class under test
+    known = None
+    for model in MODEL_TABLE.values():
+        if model.client == public_class.__name__:
+            known = model.id
+            break
+    assert known is not None, f"Model not found for client {public_class.__name__}"
+    x = public_class(model=known, nvidia_api_key="a-bogus-key")
+    assert x.model == known
+
+
+def test_known_unknown(public_class: type, known_unknown: str) -> None:
+    """
+    Test that a model in /v1/models but not in the model table will be accepted
+    with a warning.
+    """
+    with pytest.warns(UserWarning) as record:
+        x = public_class(model=known_unknown, nvidia_api_key="a-bogus-key")
+        assert x.model == known_unknown
+    assert isinstance(record[0].message, Warning)
+    assert "Found" in record[0].message.args[0]
+    assert "unknown" in record[0].message.args[0]
+
+
+def test_unknown_unknown(public_class: type) -> None:
+    """
+    Test that a model not in /v1/models and not in known model table will be
+    rejected.
+    """
+    # todo: make this work for local NIM
+    with pytest.raises(ValueError) as e:
+        public_class(model="test/unknown-unknown", nvidia_api_key="a-bogus-key")
+    assert "unknown" in str(e.value)
+
+
