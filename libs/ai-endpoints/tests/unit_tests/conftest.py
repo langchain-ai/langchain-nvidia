c2651210cd324ceaa46abb07b84f5acd28204ab0
Matthew Farrellee <matt@cs.wisc.edu>
2024-07-19 11:41:17 -0400

    add empty /v1/models fixture to ensure unit tests run without network connectivity

--- libs/ai-endpoints/tests/unit_tests/conftest.py
+++ libs/ai-endpoints/tests/unit_tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import requests_mock
 
 from langchain_nvidia_ai_endpoints import ChatNVIDIA, NVIDIAEmbeddings, NVIDIARerank
 
@@ -12,3 +13,8 @@ from langchain_nvidia_ai_endpoints import ChatNVIDIA, NVIDIAEmbeddings, NVIDIARe
 )
 def public_class(request: pytest.FixtureRequest) -> type:
     return request.param
+
+
+@pytest.fixture
+def empty_v1_models(requests_mock: requests_mock.Mocker) -> None:
+    requests_mock.get("https://integrate.api.nvidia.com/v1/models", json={"data": []})
