b27a44a8bcf313049374bf490c18935daa442ca3
Erick Friis <erick@langchain.dev>
2024-07-24 11:04:22 -0700

    x

--- libs/ai-endpoints/tests/integration_tests/test_standard.py
+++ libs/ai-endpoints/tests/integration_tests/test_standard.py
@@ -2,7 +2,6 @@
 
 from typing import Type
 
-import pytest
 from langchain_core.language_models import BaseChatModel
 from langchain_standard_tests.integration_tests import ChatModelIntegrationTests
 
@@ -10,6 +9,12 @@ from langchain_nvidia_ai_endpoints import ChatNVIDIA
 
 
 class TestNVIDIAStandard(ChatModelIntegrationTests):
-    @pytest.fixture
+    @property
     def chat_model_class(self) -> Type[BaseChatModel]:
         return ChatNVIDIA
+
+    @property
+    def chat_model_params(self) -> dict:
+        return {
+            "model": "meta/llama-3.1-8b-instruct"
+        }
--- libs/ai-endpoints/tests/unit_tests/test_standard.py
+++ libs/ai-endpoints/tests/unit_tests/test_standard.py
@@ -2,7 +2,6 @@
 
 from typing import Type
 
-import pytest
 from langchain_core.language_models import BaseChatModel
 from langchain_standard_tests.unit_tests import ChatModelUnitTests
 
@@ -10,6 +9,12 @@ from langchain_nvidia_ai_endpoints import ChatNVIDIA
 
 
 class TestNVIDIAStandard(ChatModelUnitTests):
-    @pytest.fixture
+    @property
     def chat_model_class(self) -> Type[BaseChatModel]:
         return ChatNVIDIA
+
+    @property
+    def chat_model_params(self) -> dict:
+        return {
+            "model": "meta/llama-3.1-8b-instruct"
+        }
