a3e72934ed23d4511181accf6c1d24eea92950a5
Kyle Huang <kylhuang@nvidia.com>
2024-07-29 15:10:54 -0700

    add unit test for base_url

--- libs/ai-endpoints/tests/unit_tests/test_base_url.py
+++ libs/ai-endpoints/tests/unit_tests/test_base_url.py
@@ -1,32 +1,12 @@
+import os
 import pytest
 from requests_mock import Mocker
-
-
-@pytest.mark.parametrize(
-    "base_url",
-    [
-        "bogus",
-        "http:/",
-        "http://",
-        "http:/oops",
-    ],
-)
-def test_param_base_url_negative(public_class: type, base_url: str) -> None:
-    with pytest.raises(ValueError):
-        public_class(base_url=base_url)
-
-
-@pytest.mark.parametrize(
-    "base_url",
-    ["https://integrate.api.nvidia.com/v1", "https://ai.api.nvidia.com/v1"],
-)
-def test_param_base_url_hosted(public_class: type, base_url: str) -> None:
-    client = public_class(base_url=base_url)
-    assert client._client.is_hosted
-
+from typing import Any
+from .test_api_key import no_env_var
 
 @pytest.fixture(autouse=True)
 def mock_v1_local_models(requests_mock: Mocker, base_url: str) -> None:
+    os.environ["NVIDIA_API_KEY"] = "DUMMY_KEY"
     requests_mock.get(
         f"{base_url}/models",
         json={
@@ -42,6 +22,54 @@ def mock_v1_local_models(requests_mock: Mocker, base_url: str) -> None:
         },
     )
 
+@pytest.mark.parametrize(
+    "base_url",["bogus"],
+)
+def test_create_without_base_url(public_class: type) -> None:
+   with no_env_var("NVIDIA_BASE_URL"):
+       assert public_class().base_url == "https://integrate.api.nvidia.com/v1"
+
+@pytest.mark.parametrize("base_url, param", 
+                         [("https://ai.api.nvidia.com/v1","nvidia_base_url"),
+                          ("https://ai.api.nvidia.com/v1", "base_url")])
+def test_create_with_base_url(public_class: type, base_url: str, param: str) -> None:
+    with no_env_var("NVIDIA_BASE_URL"):
+        assert public_class(**{param: base_url}).base_url == "https://ai.api.nvidia.com/v1"
+
+@pytest.mark.parametrize(
+    "base_url",["bogus"],
+)
+def test_base_url_priority(public_class: type) -> None:
+    with no_env_var("NVIDIA_BASE_URL"):
+        os.environ["NVIDIA_BASE_URL"] = "https://ai.api.nvidia.com/v1"
+        assert public_class().base_url == "https://ai.api.nvidia.com/v1"
+        assert public_class(nvidia_base_url="https://ai.api.nvidia.com/v1").base_url == "https://ai.api.nvidia.com/v1"
+        assert public_class(base_url="https://ai.api.nvidia.com/v1").base_url == "https://ai.api.nvidia.com/v1"
+        assert public_class(base_url="https://integrate.api.nvidia.com/v1", nvidia_base_url="https://ai.api.nvidia.com/v1").base_url == "https://ai.api.nvidia.com/v1"
+
+@pytest.mark.parametrize(
+
+    "base_url",
+    [
+        "bogus",
+        "http:/",
+        "http://",
+        "http:/oops",
+    ],
+)
+def test_param_base_url_negative(public_class: type, base_url: str) -> None:
+    with no_env_var("NVIDIA_BASE_URL"):
+        with pytest.raises(ValueError):
+            public_class(base_url=base_url)
+
+@pytest.mark.parametrize(
+    "base_url",
+    ["https://integrate.api.nvidia.com/v1", "https://ai.api.nvidia.com/v1"],
+)
+def test_param_base_url_hosted(public_class: type, base_url: str) -> None:
+    with no_env_var("NVIDIA_BASE_URL"):
+        client = public_class(base_url=base_url)
+        assert client._client.is_hosted
 
 @pytest.mark.parametrize(
     "base_url",
@@ -52,5 +80,6 @@ def mock_v1_local_models(requests_mock: Mocker, base_url: str) -> None:
     ],
 )
 def test_param_base_url_not_hosted(public_class: type, base_url: str) -> None:
-    client = public_class(base_url=base_url)
-    assert not client._client.is_hosted
+    with no_env_var("NVIDIA_BASE_URL") and pytest.warns(UserWarning):
+        client = public_class(base_url=base_url)
+        assert not client._client.is_hosted 
