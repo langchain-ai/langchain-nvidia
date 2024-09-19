ac7408e08f1dd99f7867411744a3bc9e9373f402
Matthew Farrellee <matt@cs.wisc.edu>
2024-09-19 13:43:25 -0400

    mark test_tool_message_histories_list_content as xfail until anthropic list content is supported

--- libs/ai-endpoints/tests/integration_tests/test_standard.py
+++ libs/ai-endpoints/tests/integration_tests/test_standard.py
@@ -2,6 +2,7 @@
 
 from typing import Type
 
+import pytest
 from langchain_core.language_models import BaseChatModel
 from langchain_standard_tests.integration_tests import ChatModelIntegrationTests
 
@@ -16,3 +17,7 @@ class TestNVIDIAStandard(ChatModelIntegrationTests):
     @property
     def chat_model_params(self) -> dict:
         return {"model": "meta/llama-3.1-8b-instruct"}
+
+    @pytest.mark.xfail(reason="anthropic-style list content not supported")
+    def test_tool_message_histories_list_content(self, model: BaseChatModel) -> None:
+        return super().test_tool_message_histories_list_content(model)
