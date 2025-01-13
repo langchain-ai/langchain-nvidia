2696f6ba22f008e8e45756def97b5a1bc05cb5f7
Matthew Farrellee <matt@cs.wisc.edu>
2025-01-13 09:51:45 -0500

    use llama 3.3 for structured output tests to address accuracy failures from 3.1

--- libs/ai-endpoints/tests/integration_tests/conftest.py
+++ libs/ai-endpoints/tests/integration_tests/conftest.py
@@ -135,7 +135,7 @@ def pytest_generate_tests(metafunc: pytest.Metafunc) -> None:
         metafunc.parametrize("completions_model", models, ids=models)
 
     if "structured_model" in metafunc.fixturenames:
-        models = ["meta/llama-3.1-8b-instruct"]
+        models = ["meta/llama-3.3-70b-instruct"]
         if model_list := metafunc.config.getoption("structured_model_id"):
             models = model_list
         if metafunc.config.getoption("all_models"):
