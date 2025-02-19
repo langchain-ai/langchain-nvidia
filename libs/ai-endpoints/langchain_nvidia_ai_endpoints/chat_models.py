c3f51fe9999944a6cf872fe774dbeaa024a3b4a2
Rashmi Pawar <raspawar@raspawar-mlt.client.nvidia.com>
2025-02-19 09:43:46 +0530

    replace imgmdr by filetype

--- libs/ai-endpoints/langchain_nvidia_ai_endpoints/chat_models.py
+++ libs/ai-endpoints/langchain_nvidia_ai_endpoints/chat_models.py
@@ -115,9 +115,9 @@ def _url_to_b64_string(image_source: str) -> str:
         elif os.path.exists(image_source):
             with open(image_source, "rb") as f:
                 image_data = f.read()
-                import imghdr
-
-                image_type = imghdr.what(None, image_data)
+                import filetype  # type: ignore
+                kind = filetype.guess(image_data)  
+                image_type = kind.extension if kind else 'unknown'
                 encoded = base64.b64encode(image_data).decode("utf-8")
                 return f"data:image/{image_type};base64,{encoded}"
         else:
