1da83de589b0a74fce92b24579bc20448b03cc69
raspawar <raspawar@nvidia.com>
2025-02-19 20:04:52 +0530

    lint fix

--- libs/ai-endpoints/tests/integration_tests/test_vlm_models.py
+++ libs/ai-endpoints/tests/integration_tests/test_vlm_models.py
@@ -183,8 +183,11 @@ def test_vlm_no_images(
     assert isinstance(response, BaseMessage)
     assert isinstance(response.content, str)
 
+
 # mark this test as xfail
-@pytest.mark.xfail(reason="Test fails when using meta/llama-3.2-11b-vision-instruct model")
+@pytest.mark.xfail(
+    reason="Test fails when using meta/llama-3.2-11b-vision-instruct model"
+)
 def test_vlm_two_images(
     vlm_model: str,
     mode: dict,
