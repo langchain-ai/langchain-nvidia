1c2bf301e8ee25beb2bd304f284f7cbbbdc6d1f5
Soumili Nandi <soumilin@nvidia.com>
2025-06-06 13:43:12 -0700

    removing repeated line

--- libs/ai-endpoints/langchain_nvidia_ai_endpoints/chat_models.py
+++ libs/ai-endpoints/langchain_nvidia_ai_endpoints/chat_models.py
@@ -296,7 +296,6 @@ class ChatNVIDIA(BaseChatModel):
                               If both max_tokens and max_completion_tokens are supplied,
                               max_completion_tokens takes precedence.
             max_completion_tokens (int): Maximum number of tokens to generate. 
-            max_completion_tokens (int): Maximum number of tokens to generate.
             top_p (float): Top-p for distribution sampling.
             seed (int): A seed for deterministic results.
             stop (list[str]): A list of cased stop words.
